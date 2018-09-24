9a88ce00e158f3479e1b6bf5e504529b0809b037
youyaa <listeningrain.cn@gmail.com>
2018-09-24 22:31:45 +0800

    提交遗漏

---
+++ ListeningrainBlog/ListeningrainBlog_service_api/src/main/java/listeningrain/cn/blog/output/data/CommonOutputData.java
@@ -0,0 +1,13 @@
+package listeningrain.cn.blog.output.data;
+
+import java.io.Serializable;
+
+/**
+ * author: listeningrain
+ * Date: 2018/9/24
+ * Time: 10:30
+ * Description:
+ */
+public class CommonOutputData implements Serializable {
+
+}
