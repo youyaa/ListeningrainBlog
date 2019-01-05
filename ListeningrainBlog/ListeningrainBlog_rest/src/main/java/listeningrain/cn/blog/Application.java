f5603e67742f4a3bd9f33a82f407821e1e3bbd0c
youyaa <listeningrain.cn@gmail.com>
2019-01-05 20:33:53 +0800

    去除多余的注解

--- ListeningrainBlog/ListeningrainBlog_rest/src/main/java/listeningrain/cn/blog/Application.java
+++ ListeningrainBlog/ListeningrainBlog_rest/src/main/java/listeningrain/cn/blog/Application.java
@@ -2,7 +2,6 @@ package listeningrain.cn.blog;
 
 import listeningrain.cn.blog.utils.StorageProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ImportResource;
@@ -17,7 +16,6 @@ import java.util.Date;
  * Description:Rest工程启动类
  */
 @SpringBootApplication
-@EnableAutoConfiguration
 @ImportResource({"classpath:application-consumer.xml"})
 @EnableConfigurationProperties(StorageProperties.class)
 public class Application {
